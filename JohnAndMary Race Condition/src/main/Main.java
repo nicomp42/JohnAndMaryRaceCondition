0797a8cff435a6426d6d21708e24f9397c3010c4
nicomp42 <nicholdw@ucmail.uc.edu>
2021-05-26 17:00:41 -0400

    First Commit

---
+++ JohnAndMary Race Condition/src/main/Main.java
@@ -0,0 +1,59 @@
+/*
+ * The starting balance is $42.
+ * We will make two deposits to the same account from two threads.
+ * John deposits $100
+ * Mary Deposits $500
+ * The final balance should be $642
+ * 
+ * If the code is not synchronized properly, we will get other balances. oopsy.
+ */
+package main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println("Making two deposits...");
+		Demo();
+		System.out.println("Main exiting");
+	}
+
+	/**
+	 * Make two deposits to the same account in separate threads. We start with
+	 * a balance of $42: John deposits $100, Mary deposits $500.
+	 * @throws Exception 
+	 */
+	private static void Demo() throws Exception {
+		// int i;
+		// We can access static class members with the class name, we don't need an object name
+		MakeDeposit.balance = 42;
+
+		/*
+		 * We need two instances of the class, each running in a separate
+		 * thread
+		 */
+		MakeDeposit johnThread = new MakeDeposit(100);
+		MakeDeposit maryThread = new MakeDeposit(500);
+
+		johnThread.start();
+		maryThread.start();
+
+		// Wait for both threads to end... it's ok to eat the exception,
+		// probably
+		try {
+			johnThread.join();
+		} catch (InterruptedException e) {
+		}
+		try {
+			maryThread.join();
+		} catch (InterruptedException e) {
+		}
+
+		// Print the final balance
+		System.out.println("The final balance is " + MakeDeposit.balance);
+	}
+}
---
+++ JohnAndMary Race Condition/src/main/MakeDeposit.java
@@ -0,0 +1,54 @@
+/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+package main;
+
+import java.util.Random;
+
+/**
+ * Simulate someone making a deposit at the ATM
+ * A simple thread class that can be launched from another class.
+ * <BR>This class inherits from the Java thread class.
+ * <BR>The run() method overrides a method with the same signature in the base class.
+ * <BR>
+ * @author Nicholson.Bill
+ */
+public class MakeDeposit extends Thread {
+
+	// balance is shared across all instances of the class. There's only one copy of the variable
+	static float balance;
+	private float depositAmount;
+/**
+ *  This method is called polymorphically from the base class.
+ */
+    public void run() {
+    	doIt();	// Make the deposit
+    }
+    /**
+     * Constructor
+     */
+    public MakeDeposit(float depositAmount) {
+    	this.depositAmount = depositAmount;
+    }
+    /**
+     * It's synchronized so it's atomic and deterministic. woo hoo. 
+     */
+    private void doIt() {
+    	// TODO: Take out the "synchronized" statement break this code.
+    	//synchronized (MakeDeposit.class){	// Only one instance of this thread can be here at a time.    	
+    		// Only one instance of this thread can be here at a time.
+	    	float temp = balance;
+	    	temp = temp + depositAmount;
+	    	// Introduce a little real-word randomness...
+	    	try {Thread.sleep(new Random().nextInt(10));} catch (Exception ex){}	// Definitely OK to eat
+	    	balance = temp;
+    	//}
+    }
+    /**
+     * Get the balance on the account
+     * @return The balance
+     */
+    public float getBalance(){return balance;}
+
+}
